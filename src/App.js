199dce46b9af1d7fbabaa7ab36ae666e6c4a360f
Raj Chavda <raj.theappideas@gmail.com>
2022-12-02 12:06:07 +0530

    second commit set up complete & add all files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+import { lazy, Suspense } from "react";
+import Lottie from "react-lottie";
+import loading from "./assets/animations/loading.json";
+
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
-  return <div>React App</div>;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loading,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <BrowserRouter>
+      <Toaster />
+      <Suspense
+        fallback={() => {
+          <Lottie
+            options={defaultOptions}
+            height={300}
+            width={300}
+            style={{
+              position: "absolute",
+              top: "1/2",
+              left: "1/2",
+              transform: "traslate(50%,50%)",
+              width: "100%",
+              height: "100%",
+            }}
+          />;
+        }}
+      />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet title="Home" />
+      <div>home</div>
+    </>
+  );
+};
+
+export default Home;
